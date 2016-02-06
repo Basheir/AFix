7bc1bd3e6190f372beb2c3224ef1d31ffdc6e5e2
Basheir <php0@hotmail.com>
2016-02-06 20:22:50 +0300

    تمكين الفلترة حسب نوع الجهاز

--- json/devices/getCommentAutoCompleat.php
+++ json/devices/getCommentAutoCompleat.php
@@ -11,11 +11,11 @@ include('../../config.php');
 //    $infoList = $db->where("Comment LIKE '$_POST[$p]' ");
 //
 //}
- 
+
 $infoList = $db->get('devices');
 
 
 echo $db->jsonBuilder()->get('devices', null, 'Comment');
-
+ 
 
 ?>
