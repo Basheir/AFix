de73b961e0783d1aa81c95b0a1632a69c4175dee
Basheir <php0@hotmail.com>
2016-03-17 22:24:24 +0300

    DataBase:Change

--- js/MyJs.js
+++ js/MyJs.js
@@ -435,7 +435,7 @@ function showListDevices(d) {
 
 
         //console.log($('#DevicesListTemplate'));
-
+ 
 
         $('*[data-poload]').popover({
             "html": true,
@@ -454,7 +454,7 @@ function showListDevices(d) {
             });
         });
 
- 
+
         function details_in_popup(link, div_id) {
 
 
