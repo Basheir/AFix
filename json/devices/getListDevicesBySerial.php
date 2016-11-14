6336731247c5740aac5bcfd7216041aece73c651
PHP0 <php0@hotmail.com>
2016-11-14 09:23:34 +0300

    اكمال تلقائي للبحث

---
+++ json/devices/getListDevicesBySerial.php
@@ -0,0 +1,33 @@
+<?php
+//*
+
+header('Content-Type: application/json');
+
+
+$resultData = array();
+@$searchBy = $_GET['by'];
+@$Parm = $_GET['Parm'];
+@$getAll = $_GET['getAll'];
+$limit = null;
+@$lastID = $_GET['lastID'];
+
+include('../../config.php');
+
+include('../../json/getSetting.php');
+
+
+
+
+
+$db->where('Serial','%'.$_GET['c'], "LIKE");
+
+
+$devicesList = $db->JsonBuilder()->get('devices');
+
+
+echo $devicesList;
+//echo $db->getLastQuery();
+
+
+?>
+
