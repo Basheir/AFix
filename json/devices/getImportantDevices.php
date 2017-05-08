669425852ab3c6c526fa34fd48028905042c55dc
PHP0 <php0@hotmail.com>
2017-05-08 10:43:56 +0300

    Add Router JS

---
+++ json/devices/getImportantDevices.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * احضار معلومات الاجهزة وانواعها لعرضها في قائمة التنبيه
+ */
+
+
+include('../../config.php');
+include('../../Class/functions.php');
+
+$resultImploud = array();
+
+$devicesN = $db->rawQuery('
+
+    SELECT
+  COUNT(devices.Finsh) AS C,
+  devices.IDTypeDevice AS T,
+  typedevices.NameDevices AS N
+FROM
+  devices
+  INNER JOIN typedevices ON (devices.IDTypeDevice = typedevices.ID)
+
+  WHERE   devices.important = "1"
+
+
+GROUP BY
+  devices.IDTypeDevice
+
+');
+
+
+foreach ($devicesN as $v) {
+
+
+    $resultImploud[] = array('type' => $v['N'], 'count' => $v['C'], 'IDTypeDevice' => $v['T']);
+
+}
+
+
+echo json_encode($resultImploud);
+
+
+?>
+
